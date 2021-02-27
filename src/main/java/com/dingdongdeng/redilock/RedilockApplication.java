ef2c590c5411e0a316bf72a73a25cdbc1ab43c2e
jim <jim@gccompany.co.kr>
2021-02-27 16:30:16 +0900

    project init

---
+++ src/main/java/com/dingdongdeng/redilock/RedilockApplication.java
@@ -0,0 +1,13 @@
+package com.dingdongdeng.redilock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedilockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedilockApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dingdongdeng/redilock/RedilockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dingdongdeng.redilock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedilockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
